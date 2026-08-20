222aacb9b9a5a7e848c50d9457f453ba8aae0a02
GuilhermeTavares13 <gui.tavares2002.contato@gmail.com>
2026-08-20 06:42:27 -0300

    update files

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,15 +1,26 @@
 import { Pressable, StyleSheet, View, Text } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
+import { getFormattedDate } from "../../util/date";
+import { useNavigation } from "@react-navigation/native";
 
-function ExpenseItem({ description, date, amount }) {
-    return <Pressable>
+function ExpenseItem({ id, description, date, amount }) {
+
+    const navigation = useNavigation();
+
+    function expensePressHandler() {
+        navigation.navigate('ManageExpense', {
+            expenseId: id
+        });
+    }
+
+    return <Pressable onPress={expensePressHandler} style={({pressed}) => pressed && styles.pressed}>
         <View style={styles.expenseItem}>
             <View>
                 <Text style={[styles.textBase, styles.description]}>{description}</Text>
-                <Text style={styles.textBase}>{date.toString()}</Text>
+                <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
             </View>
             <View style={styles.amountContainer}>
-                <Text style={[styles.textBase, styles.amount]}>{amount}</Text>
+                <Text style={[styles.textBase, styles.amount]}>{amount.toFixed(2)}</Text>
             </View>
         </View>
     </Pressable>
@@ -18,6 +29,9 @@ function ExpenseItem({ description, date, amount }) {
 export default ExpenseItem;
 
 const styles = StyleSheet.create({
+    pressed: {
+        opacity: 0.5
+    },
     expenseItem: {
         padding: 12,
         marginVertical: 8,
@@ -46,6 +60,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 4,
+        minWidth: 80
     },
     amount: {
         color: GlobalStyles.colors.primary500,
--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -3,13 +3,13 @@ import { GlobalStyles } from "../../constants/styles";
 
 function ExpensesSummary({expenses, periodName}) {
 
-    const expensesSum = expenses.reduce((sum, expense) => {
+    const expensesSum = expenses?.reduce((sum, expense) => {
         return sum + expense.amount
-    }, 0);
+    }, 0) || 0;
 
     return <View style={styles.container}>
                 <Text style={styles.period}>{periodName}</Text>
-                <Text style={styles.sum}>${expensesSum.toFixed(2)}</Text>
+                <Text style={styles.sum}>${expensesSum?.toFixed(2)}</Text>
             </View> 
 }
 

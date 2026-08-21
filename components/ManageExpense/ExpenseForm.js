17e982905a05ddce6ed61d9711021255f3196a24
GuilhermeTavares13 <gui.tavares2002.contato@gmail.com>
2026-08-21 06:19:28 -0300

    Update files

---
+++ components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,53 @@
+import { StyleSheet, Text, View } from "react-native";
+import Input from "./Input";
+import { useState } from "react";
+
+function ExpenseForm() {
+    const [amountValue, setAmountValue] = useState('');
+
+
+    function amountChangedHandler(enteredText) {
+        setAmountValue(enteredText);
+    }
+
+    return <View style={styles.form}>
+        <Text style={styles.title}>Your Expense</Text>
+        <View style={styles.inputsRow}>
+            <Input label="Amount"  style={styles.rowInput} textInputConfig={{
+                keyboardType: 'decimal-pad',
+                onChangeText: amountChangedHandler,
+                value: amountValue
+            }}/>
+            <Input label="Date" style={styles.rowInput} textInputConfig={{
+                placeholder: 'YYYY-MM-DD',
+                maxLength: 10,
+                onChangeText: () => {}
+            }}/>
+        </View>
+        <Input label="Description" textInputConfig={{
+            multiline: true
+        }}/>
+    </View>;
+}
+
+export default ExpenseForm;
+
+const styles = StyleSheet.create({
+    form: {
+        marginTop: 40
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+        marginVertical: 24,
+        textAlign: 'center'
+    },
+    inputsRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    rowInput: {
+        flex: 1
+    }
+});
